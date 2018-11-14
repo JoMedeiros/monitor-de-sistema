63a550e46ee454b6222e15b093067cf6acc45db1
jimmy <jimmy@pop-os.localdomain>
2018-11-14 01:55:21 -0300

    order by cpu or memory usage

--- monitor.py
+++ monitor.py
@@ -22,15 +22,17 @@ def main(stdscr):
     #pFltPad = curses.newpad(100, 100) # container to show the page faults
 
     key = curses.KEY_RIGHT
-    msg='Teste'
-
+    ord_by = 'pid'
+    fields = 'pid,%mem,min_flt,maj_flt,comm'
+    f = os.popen('ps -eo '+fields)
     pFltInit = 1 # page fault list init to be shown
     """--  Main Loop  --"""
     while key != 27:
-        win.addstr(int(heigh/2), 3, msg)
         prevKey = key # Previous key pressed
-        win.addstr(0, 40, 'Pressione Esc para sair')
-        win.addstr(1, 40, 'Pressione as teclas ↑ ou ↓ para navergar')
+        win.addstr(0, 40, 'Teclas:')
+        win.addstr(1, 40, 'UP/DOWN - navegar\tEsc - Sair')
+        win.addstr(2, 40, 'm - ordena por uso de memoria')
+        win.addstr(3, 40, 'c - ordena por uso de cpu')
 
         """---   Collecting Memory info   ---"""
         mem = psutil.virtual_memory()
@@ -38,16 +40,36 @@ def main(stdscr):
         for x in range(50):
             win.addstr(19,2+x,' ', curses.color_pair(1))
         for x in range(int(mem.percent/2)):
-            win.addstr(19,2+x,'█', curses.color_pair(1+int(mem.percent/25)))
+            win.addstr(19,2+x,'@', curses.color_pair(1+int(mem.percent/25)))
 
-        """------       Collecting Page fault data      ------"""
-        f = os.popen('ps -eo pid,min_flt,maj_flt,cmd')
+        """------       Collecting Processes Data      ------"""
+        if ord_by == 'mem':
+            f = os.popen('ps -eo '+fields+' --sort -rss')
+        elif ord_by == 'cpu':
+            f = os.popen('ps -eo '+fields+' --sort -pcpu')       
+        else:
+            f = os.popen('ps -eo '+fields)
         table = f.read()
-        lines = table.split(sep='\n')
-        win.addstr(0, 3, lines[0])
-        win.addstr(1, 3, lines[pFltInit], curses.color_pair(5))
-        for i in range(2,heigh-4):
-            win.addstr(i, 3, lines[i + pFltInit])
+        lines = table.split('\n')
+        win.addstr(0, 0, lines[0].split()[0].rjust(6))# PID label
+        win.addstr(0, 6, 'comando'.rjust(10))# PID label
+        #selected
+        win.addstr(1, 0, lines[pFltInit].split()[0].rjust(6), curses.color_pair(5))
+        win.addstr(1, 7, lines[pFltInit].split()[4], curses.color_pair(5))
+        #others
+        for i in range(1,heigh-4):
+            l = lines[i + pFltInit].split()
+            win.addstr(i+1, 0, l[0].rjust(6))
+            win.addstr(i+1, 7, l[4])
+
+        """- Selected Process information -"""
+        ff = os.popen('ps -p ' + lines[pFltInit].split()[0] + ' -eo pid,min_flt,maj_flt')
+        sel = ff.read()
+        win.addstr(5, 40, 'Process ID: ' + lines[pFltInit].split()[0].rjust(6))
+        win.addstr(6, 40, 'Uso de memoria:    ' + lines[pFltInit].split()[1].rjust(8) + '%')
+        win.addstr(7, 40, 'minor page faults: ' + lines[pFltInit].split()[2].rjust(8))
+        win.addstr(8, 40, 'major page faults: ' + lines[pFltInit].split()[3].rjust(8))
+
 
         """---   Getting the user input   ---"""
         event = win.getch()
@@ -59,6 +81,11 @@ def main(stdscr):
         elif key == curses.KEY_UP:
             pFltInit = max(pFltInit - 1, 1)
             key = prevKey
+        elif key == 109 or key == 77:
+            ord_by = 'mem'
+        elif key == 67 or key == 99:
+            ord_by = 'cpu'
+
 
     win.keypad(False)
     curses.echo()
